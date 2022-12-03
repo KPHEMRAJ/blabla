cf83389546789e00f6ae6addf3979823e2846e64
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2022-12-03 19:59:15 +0530

    Update Nanswap price.py

--- Nanswap price.py
+++ Nanswap price.py
@@ -5,7 +5,7 @@ URL = "https://api.nanswap.com/v1/get-estimate?from=XNO&to=BAN&amount="+'10'
 
 response = requests.get(URL)
 userdata = json.loads(response.text)
-to = userdata["amountTo"]/userdata["amountFrom"]
+price= userdata["amountTo"]/userdata["amountFrom"]
    
 
-print(to)
+print(price)
