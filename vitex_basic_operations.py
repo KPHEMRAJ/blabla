9a5c55a6a7ac5cf942a3397c35c95e2957c65b0b
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2022-12-07 11:51:34 +0530

    Update and rename vitex_create_order.py to vitex_basic_operations.py

---
+++ vitex_basic_operations.py
@@ -0,0 +1,54 @@
+#import time
+import requests
+import hashlib
+#import calendar
+import datetime
+import hmac
+#import binascii
+
+def create_sha256_signature(key, message):
+
+    #byte_key = binascii.unhexlify(key)
+    byte_key = str.encode(key)
+    message = message.encode()
+    return hmac.new(byte_key, message, hashlib.sha256).hexdigest()
+
+
+def time_stamp():
+
+    presentDate = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(presentDate))
+    timestamp = timestamp*1000
+    return timestamp
+
+
+def create_order(amount,api_key,secret_key,price,side,symbol,url):
+    message = "amount="+amount+"&key=" + \
+        api_key+"&price="+price+"&side="+side+"&symbol="+symbol+"&timestamp=" + \
+        str(time_stamp()) 
+    s = create_sha256_signature(secret_key,message)
+    payload = message+"&signature="+str(s)
+    headers = {}
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+
+def get_orders(address,url):
+    uurl=url+"s?address="+address
+    payload={}
+    headers={}
+    response = requests.request("GET", uurl, headers=headers, data=payload)
+    print(response.text)
+
+secret_key = ""
+api_key = ""
+amount="1000"
+price="0.051"
+side="0"
+symbol="BAN-001_VITE"
+order_id=""
+url = "https://api.vitex.net/api/v2/order"
+address = "vite_53376e73f8cad15002c9ef4d5a7e96ceee13f7150dc18e7965"
+create_order(amount,api_key,secret_key,price,side,symbol,url)
+get_orders(address,url)
+
+
