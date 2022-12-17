eb60aad68aeb429d11fe0a5d71cb90f8cdf74c09
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2022-12-17 10:38:54 +0530

    Update vitex_basic_operations.py

--- vitex_basic_operations.py
+++ vitex_basic_operations.py
@@ -6,6 +6,7 @@ import datetime
 import hmac
 #import binascii
 
+
 def create_sha256_signature(key, message):
 
     #byte_key = binascii.unhexlify(key)
@@ -22,34 +23,42 @@ def time_stamp():
     return timestamp
 
 
-def create_order(amount,api_key,secret_key,price,side,symbol,url):
-    url=url+"order"
-    message = "amount="+amount+"&key=" + \
-        api_key+"&price="+price+"&side="+side+"&symbol="+symbol+"&timestamp=" + \
-        str(time_stamp()) 
-    s = create_sha256_signature(secret_key,message)
-    payload = message+"&signature="+str(s)
-    headers = {}
-    response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+class vitex:
 
-def get_orders(address,url):
-    url=url+"orders?address="+address
-    payload={}
-    headers={}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.text)
-
-secret_key = ""
-api_key = ""
-amount="1000"
-price="0.051"
-side="0"
-symbol="BAN-001_VITE"
-order_id=""
-url = "https://api.vitex.net/api/v2/"
-address = "vite_53376e73f8cad15002c9ef4d5a7e96ceee13f7150dc18e7965"
-create_order(amount,api_key,secret_key,price,side,symbol,url)
-get_orders(address,url)
+    secret_key = ""
+    api_key = ""
+    amount = "1000"
+    price = "0.051"
+    side = "0"
+    symbol = "BAN-001_VITE"
+    order_id = ""
+    url = "https://api.vitex.net/api/v2/"
+    address = "vite_53376e73f8cad15002c9ef4d5a7e96ceee13f7150dc18e7965"
+    def create_order(self):
+        self.message = "amount="+self.amount+"&key=" + \
+            self.api_key+"&price="+self.price+"&side="+self.side+"&symbol="+self.symbol+"&timestamp=" + \
+            str(time_stamp())
+        self.url=self.url+"order"
+        s = create_sha256_signature(self.secret_key, self.message)
+        payload = self.message+"&signature="+str(s)
+        headers = {}
+        response = requests.request("POST", self.url, headers=headers, data=payload)
+        print(response.text)
+        self.url=self.url.replace("order",'')
 
 
+    def get_orders(self):
+        self.url = self.url+"orders?address="+self.address
+
+        payload = {}
+        headers = {}
+        response = requests.request("GET", self.url, headers=headers, data=payload)
+        print(response.text)
+        s = "orders?address="+self.address
+        self.url=self.url.replace(s,'')
+
+
+v1=vitex()
+
+v1.create_order()
+v1.get_orders()
