05fcf79839470682305f51cfda1f2db51bfdd9d1
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-03 18:52:38 +0530

    Add files via upload

---
+++ js/get_conversion.js
@@ -0,0 +1,12 @@
+const {ViteAPI} = require('@vite/vitejs');
+const {HTTP_RPC } = require('@vite/vitejs-http')
+const {get_conversion}=require('./vitcswap_abi')
+const contractAbi= get_conversion;
+const contractAddress="vite_29ae0b9f951323b3bfe9bb8251bba2830eddacf51631630495";
+const viteProvider =new ViteAPI(new HTTP_RPC("https://node.vite.net/gvite"), () => {})
+async function vite_conversion(ca1,ca2,am){
+    const methodName = 'getConversion';
+let data=viteProvider.queryContractState({ address: contractAddress, abi: contractAbi,methodName, params:[ca1,ca2,am] })
+let resp1=await data
+return resp1}
+module.exports={vite_conversion}
