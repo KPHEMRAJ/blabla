d705b0fac7810f55ebac592aec32f45e323a4e2b
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-03 18:50:26 +0530

    Update get_liquidity.js

--- js/get_liquidity.js
+++ js/get_liquidity.js
@@ -1,34 +1,7 @@
-const {
-    accountBlock, ViteAPI, wallet, constant, communication
-} = require('@vite/vitejs');
+const { ViteAPI} = require('@vite/vitejs');
 const {HTTP_RPC } = require('@vite/vitejs-http')
-const {abi} =require('@vite/vitejs');
-const { Constants } = require('discord.js');
-const contractAbi={
-    name:'getLiquidity',
-     type:'function',
-      "constant":false,
-    "payable":false,
-    "stateMutability":"nonpayable",
-    inputs:[{"type":'tokenId', 
-    "name": 'token',"internalType": "tokenId"}],
-	outputs:[{"name":"","type":"uint256",
-                "internalType": "uint256"},
-            {"name":"","type":"uint256",
-                "internalType": "uint256"
-
-            },{
-                "name":"","type":"uint256",
-                "internalType": "uint256"
-            },
-            {
-                "name":"","type":"uint256",
-                "internalType": "uint256"
-            },
-        {"name":"","type":"tokenId",
-                "internalType": "tokenId"}
-            ]
-}
+const {get_liquidity}=require('./vitcswap_abi.js')
+const contractAbi=get_liquidity;
 const methodName = 'getLiquidity';
 const contractAddress="vite_29ae0b9f951323b3bfe9bb8251bba2830eddacf51631630495";
 const viteProvider =new ViteAPI(new HTTP_RPC("https://node.vite.net/gvite"), () => {})
