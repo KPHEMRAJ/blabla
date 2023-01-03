bdfa668e77b2b6b065ef3d5e641525393db4dd12
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-03 21:53:50 +0530

    Update send_vite.js

--- js/send_vite.js
+++ js/send_vite.js
@@ -1,24 +1,38 @@
 
-const {
-    accountBlock, ViteAPI, wallet
-} = require('@vite/vitejs');
+const {accountBlock, ViteAPI} = require('@vite/vitejs');
 const {HTTP_RPC } = require('@vite/vitejs-http')
 
 const viteProvider =new ViteAPI(new HTTP_RPC("https://node.vite.net/gvite"), () => {})
 
 
 const AccountBlock = accountBlock.AccountBlock;
-
+async function send(from,to,token_id,amounts){
 const myAccountBlock = new AccountBlock({
     blockType: 2,
-    address: 'vite_21dac1fcb30529ee238e67b429459cb56ef34fc023dc724b67',
-    toAddress: 'vite_d4d963fa23f035b11d529f1fffd9606706a057f2e93131f123',
-    tokenId: 'tti_541b25bd5e5db35166864096',
-    amount: '1'
+    address:from,
+    toAddress:to,
+    tokenId:token_id,
+    amount:amounts
 });
-myAccountBlock.setProvider(viteProvider).setPrivateKey('Passphrase in hex');
-myAccountBlock.autoSend().then(data => {
-    console.log('success', data);
-}).catch(err => {
-    console.warn(err);
-});
+myAccountBlock.setProvider(viteProvider).setPrivateKey('private key in hex');
+let data=myAccountBlock.autoSend()
+try{await data}
+catch(e)
+{ return e}
+}
+module.exports={send}
+
+/*
+const {send}=require('./send.js')
+async function foo()
+{let from="vite_53376e73f8cad15002c9ef4d5a7e96ceee13f7150dc18e7965";
+let to="vite_d4d963fa23f035b11d529f1fffd9606706a057f2e93131f123";
+let token_id="tti_541b25bd5e5db35166864096";
+let amount=1
+ amount=amount.toString();
+    let data=await send(from,to,token_id,amount);
+    if(data!=undefined)
+console.log(data)
+}
+foo()
+*/
