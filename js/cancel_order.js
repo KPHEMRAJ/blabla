72c63d94e506f62d625047932ae03c16d941ccdf
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-03 21:36:40 +0530

    Update cancel_order.js

--- js/cancel_order.js
+++ js/cancel_order.js
@@ -1,11 +1,9 @@
-
-const {
-    accountBlock, ViteAPI, wallet
-} = require('@vite/vitejs');
+const {accountBlock, ViteAPI, abi} = require('@vite/vitejs');
 const {HTTP_RPC } = require('@vite/vitejs-http')
-const {abi} =require('@vite/vitejs');
+
+const { data } = require('config-json');
 const viteProvider =new ViteAPI(new HTTP_RPC("https://node.vite.net/gvite"), () => {})
-function cancel_order(order_id,token_id){
+async function cancel_order(order_id,token_id){
 result=abi.encodeFunctionCall({
 	'type': 'function', 'name': 'CancelOrderByTransactionHash', 'inputs': [ { 'name': 'sendHash', 'type': 'bytes32' } ]},[order_id]);
 var base_64 = Buffer.from(result, 'hex').toString('base64')
@@ -18,11 +16,23 @@ const myAccountBlock = new AccountBlock({
     tokenId:token_id,
     data:base_64
 });
-myAccountBlock.setProvider(viteProvider).setPrivateKey('passphrase in hex');
-myAccountBlock.autoSend().then(data => {
-    console.log('success', data);
-}).catch(err => {
-    console.warn(err);
-});}
+myAccountBlock.setProvider(viteProvider).setPrivateKey('private key in hex');
+let data=myAccountBlock.autoSend()
+try
+{ await data;}
+catch(e)
+{return e;}}
 /*cancel_order("626ceed9b2d5c08164a9f95996770aae8fe2a48d8f84a40880ed64031f9a5c51","tti_f9bd6782f966f899d74d7df8")*/
 module.exports={cancel_order}
+
+/*
+const {cancel_order}=require('./cancel_order.js')
+async function foo()
+{
+    let data=await cancel_order("626ceed9b2d5c08164a9f95996770aae8fe2a48d8f84a40880ed64031f9a5c51","tti_f9bd6782f966f899d74d7df8")
+    if(data!=undefined)
+    console.log(data.error)
+    
+}
+foo()
+*/
