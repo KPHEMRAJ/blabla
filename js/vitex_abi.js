13ad6da0b36a7f4865cdfd0dab731942db7ca549
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-03 23:29:44 +0530

    Add files via upload

---
+++ js/vitex_abi.js
@@ -0,0 +1,21 @@
+const withdraw={
+    
+	'type': 'function',
+    'name': 'DexFundUserWithdraw',
+    'inputs': [ 
+        {
+        'name': 'token',
+        'type': 'tokenId' 
+        },
+        {
+         'name': 'amount',
+        'type': 'uint256' 
+        } 
+        ]
+}
+
+const deposit={
+	'type': 'function',
+    'name': 'DexFundUserDeposit',
+    'inputs': [] 
+    }
