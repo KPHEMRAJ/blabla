61107091b5eaf03ac938b16c0a9c3e7598a86569
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-08 17:39:33 +0530

    Update and rename vitex+coinex.py to vitex_coinex.py

--- py/vitex_coinex.py
+++ py/vitex_coinex.py
@@ -30,52 +30,53 @@ class coinex:
     secret_key = ""
     url = "https://api.coinex.com/v1/"
 
-    def depth(self):
-        url = self.url+"market/depth?market=CETUSDT&merge=0.00001"
+    def depth(self,market,limit):
+        url = self.url+"market/depth?market="+market+"&merge=0.00001&limit="+limit
         response = requests.request("GET", url)
-        print(response.text)
+        return response.json()
 
     def balance(self):
         params = {
-        "access_id":self.access_id,
-        "tonce":str(time_stamp())
+            "access_id": self.access_id,
+            "tonce": str(time_stamp())
         }
         url = self.url+"balance/info?access_id="+self.access_id+"&tonce=" + \
             str(time_stamp())
-        
+
         headers = {"authorization": create_md5_sign(params, self.secret_key)}
         response = requests.request("GET", url, headers=headers)
         print(response.text)
-    
+
     def fees(self):
         url = self.url+"account/market/fee?"
-        params={
-        "access_id":self.access_id,
-        "tonce":str(time_stamp()),
-        "market":"VITEUSDT"
+        params = {
+            "access_id": self.access_id,
+            "tonce": str(time_stamp()),
+            "market": "VITEUSDT"
         }
         for a in params:
-            url=url+a+'='+params[a]+'&' 
-        url=url[:-1]
+            url = url+a+'='+params[a]+'&'
+        url = url[:-1]
         headers = {"authorization": create_md5_sign(params, self.secret_key)}
         response = requests.request("GET", url, headers=headers)
         print(response.text)
 
     def withdraw(self):
         url = self.url+"balance/coin/withdraw"
-        data={
-        "access_id": self.access_id,
-        "tonce": str(time_stamp()),
-        "coin_type":"BAN",
-        "coin_address":"ban_3sef57qz717a9z1ochzhrhkiiyeofozhgcfhrspoq8o9m4xaio7kk48jpdwj",
-        "actual_amount":"1001"
+        data = {
+            "access_id": self.access_id,
+            "tonce": str(time_stamp()),
+            "coin_type": "BAN",
+            "coin_address": "ban_3sef57qz717a9z1ochzhrhkiiyeofozhgcfhrspoq8o9m4xaio7kk48jpdwj",
+            "actual_amount": "1001"
         }
         headers = {"authorization": create_md5_sign(data, self.secret_key)}
-        response = requests.request("POST", url, headers=headers,json=data)
+        response = requests.request("POST", url, headers=headers, json=data)
         print(response.text)
-    
+
     def deposit_address(self):
-        url = self.url+"balance/deposit/address/vite?" + "access_id="+self.access_id+"&tonce="+str(time_stamp())
+        url = self.url+"balance/deposit/address/vite?" + \
+            "access_id="+self.access_id+"&tonce="+str(time_stamp())
         params = {
             "access_id": self.access_id,
             "tonce": str(time_stamp())
@@ -95,20 +96,18 @@ class coinex:
             "price": "0.006596"
         }
         headers = {"authorization": create_md5_sign(data, self.secret_key)}
-        response = requests.request("POST", url, headers=headers,json=data)
+        response = requests.request("POST", url, headers=headers, json=data)
         print(response.text)
 
     def is_maintanace(self):
-        url=self.url+"common/maintain/info"
+        url = self.url+"common/maintain/info"
         response = requests.request("GET", url)
         print(response.text)
-        url=self.url+"common/temp-maintain/info"
+        url = self.url+"common/temp-maintain/info"
         response = requests.request("GET", url)
         print(response.text)
 
 
-
-        
 class vitex:
 
     secret_key = ""
@@ -140,44 +139,41 @@ class vitex:
         response = requests.request(
             "GET", url, headers=headers, data=payload)
         print(response.text)
-    
-    def depth(self):
+
+    def depth(self,symbol):
         url = self.url+"depth"
-        params = {"symbol": "VITE_USDT-000"}
+        params = {"symbol": symbol}
         response = requests.request("GET", url, params=params)
-        print(response.text)
-    
+        return response.json()
+
     def order_limit(self):
-        url=self.url+"limit"
+        url = self.url+"limit"
         response = requests.request("GET", url)
         print(response.text)
-        dict=response.json()
-        data=dict["data"]
-        minAmount=data["minAmount"]
+        dict = response.json()
+        data = dict["data"]
+        minAmount = data["minAmount"]
         print(minAmount["USDT-000"])
 
     def exchange_balance(self):
         url = self.url+"balance"
         params = {
             "address": "vite_d4d963fa23f035b11d529f1fffd9606706a057f2e93131f123"}
-        response = requests.request("GET",url,params=params)
+        response = requests.request("GET", url, params=params)
         print(response.text)
 
 
-
-
-
 v1 = vitex()
-#v1.create_order()
-#v1.order_history()
-#v1.depth()
-#v1.order_limit()
-#v1.exchange_balance()
+# v1.create_order()
+# v1.order_history()
+# v1.depth()
+# v1.order_limit()
+# v1.exchange_balance()
 c1 = coinex()
-#c1.depth()
-#c1.balance()
-#c1.fees()
-#c1.withdraw()
-#c1.deposit_address()
-#c1.create_order()
-#c1.is_maintanace()
+# c1.depth()
+# c1.balance()
+# c1.fees()
+# c1.withdraw()
+# c1.deposit_address()
+# c1.create_order()
+# c1.is_maintanace()
