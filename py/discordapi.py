508f9ed411c1aa06f111641eeb3e80229639541f
KPHEMRAJ <65623621+KPHEMRAJ@users.noreply.github.com>
2023-01-08 17:38:27 +0530

    Update discordapi.py

--- py/discordapi.py
+++ py/discordapi.py
@@ -1,12 +1,9 @@
 import requests
 import time
 url = "https://discord.com/api/v10/channels/channel_id/messages"
-
-payload = {
-    "content": "hi"
-}
-header = {"authorization": "Bot Token"
-          }
-while True:
+def tell(message):
+    payload = {"content": message}
+    header = {
+        "authorization": "Bot token"}
     r = requests.request("POST", url, data=payload, headers=header)
-    time.sleep(10)
+
